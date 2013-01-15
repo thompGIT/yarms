556e703fa225ab146289eacf6c015d56f792763a
James Thompson <jamesdavidthompson@gmail.com>
2013-01-14 19:53:36 -0800

    Initial checking. Only the ajax() function is included so far.

---
+++ js/yarms.js
@@ -0,0 +1,22 @@
+/******************************************************************************
+ debug
+******************************************************************************/
+var g_DEBUG = 1
+function debug(msg) {
+    if(g_DEBUG) {
+        console.log('[' + msg + ']');
+    }
+}
+
+/******************************************************************************
+ Utility Functions
+******************************************************************************/
+function ajax(url) {
+    var xmlhttp = new XMLHttpRequest();
+    debug("AJAX: " + url);
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send();
+    var resp = xmlhttp.responseText;
+    debug("AJAX: " + resp);
+    return resp;
+}
