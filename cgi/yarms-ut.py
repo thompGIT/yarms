0f142f25c455fa56506762c8dd2e982e2a1f30d0
James Thompson <jamesdavidthompson@gmail.com>
2013-01-14 19:35:53 -0800

    Initial checkin. Includes a generator for random employee names.

---
+++ cgi/yarms-ut.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+#
+# YARM Unit Test Helper
+#
+
+import sys
+import random
+import DbSqlite
+
+#------------------------------------------------------------------------------
+# MAIN
+#------------------------------------------------------------------------------
+
+db = DbSqlite.DbSqlite('../yarms.db')
+num_args = len(sys.argv)
+
+if not ((num_args == 2) or (num_args == 3)): 
+    print 'Usage: ' + str(sys.argv[0]) + ' <command> [parameter]'
+    print ' Commands: genEmployees [count]'
+    sys.exit(1)
+
+if sys.argv[1] == 'genEmployees':
+    if num_args != 3:
+        print 'Usage: ' + str(sys.argv[0]) + ' genEmployees [count]'
+        sys.exit(1)
+
+    num_employees = int(sys.argv[2])
+    firstNames = ['James','John','Robert','Michael','William','David','Richard','Charles','Joseph','Thomas']
+    middleNames = ['Deon','Josiah','Rico','Trey','Dewitt','Fritz','Milford','Jerold','Napoleon','Jamaal','Yong']
+    lastNames = ['Smith','Johnson','Williams','Jones','Brown','Davis','Miller','Wilson','Moore','Taylor','Martin']
+
+    print 'Removing old employees...'
+    db.delAllEmployees()
+    print 'Generating ' + str(num_employees) + ' employees...'
+    for i in range(num_employees):
+        name = random.choice(firstNames) + ' ' + random.choice(middleNames) + ' ' + random.choice(lastNames)
+        empId = 'SIG-' + str(i)
+        title = 'E0' + str(random.randint(1,6))
+        supervisor = random.randint(1,num_employees-1) % 5
+        print '[' + title + '] [' + empId + '] [' + str(supervisor) + '] ' + name
+        db.addEmployee(name,empId,title,supervisor)
+        
+
+
