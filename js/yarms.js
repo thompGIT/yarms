1ef75a09bcc6065556d992202527dce389c08656
James Thompson <jamesdavidthompson@gmail.com>
2013-01-23 19:22:38 -0800

    relocated all js to this file. creation of datastores working.

--- js/yarms.js
+++ js/yarms.js
@@ -1,6 +1,7 @@
-/*****************************************************************************
- debug
+/******************************************************************************
+ Utility Functions
 ******************************************************************************/
+
 var g_DEBUG = 1
 function debug(msg) {
     if(g_DEBUG) {
@@ -8,9 +9,6 @@ function debug(msg) {
     }
 }
 
-/******************************************************************************
- Utility Functions
-******************************************************************************/
 function ajax(url) {
     var xmlhttp = new XMLHttpRequest();
     debug("AJAX: " + url);
@@ -21,7 +19,50 @@ function ajax(url) {
     return resp;
 }
 
-dojo.ready(function() {
-    debug('Testing 1..2..3..');
+/******************************************************************************
+ Global Variables
+******************************************************************************/
+var DataStore_Employees;
+
+require(["dojo/ready",
+         "dojo/data/ItemFileReadStore",
+         "dojox/data/KeyValueStore", 
+         "dojox/grid/DataGrid", 
+         "dijit/form/FilteringSelect"
+], function (ready, Select) {
+
+    //-- Initial startup
+    ready(function() {
+        BuildDataStores();
+        InitEmployeeSelector();
+    });
+    
+    //-- Build Data Stores
+    function BuildDataStores() {
+        var i, resp, tokens, data_list;
+            
+        // Create the employees datastore with all non-null names returned
+        resp = ajax('./cgi/jsIface.py?op=getEmployeeNames');
+        if (resp != '/n') {
+            i = 0;
+            tokens = resp.split("\n");
+            data_list = new Array();
+            while (i<tokens.length) {
+                if (tokens[i].length > 0)
+                    data_list.push({'name':tokens[i]});
+                i++;
+            }
+            DataStore_Employees = new dojox.data.KeyValueStore({data: data_list});
+        }
+    }
+
+    //-- Update the table with live information from the database
+    function InitEmployeeSelector() {            
+        var s = new dijit.form.FilteringSelect({
+            store: DataStore_Employees
+        }, "target_select");
+        s.startup();
+    };
+    
 });
 
